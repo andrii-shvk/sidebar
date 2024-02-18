76dc3093549bf9e81bb8344e7c0766fde2c07aa9
Andrii Shevchuk <supamercron@gmail.com>
2024-02-18 12:43:42 -0500

    add NavLink and change active className

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,26 +1,31 @@
 import "../App.css";
 import { SidebarData } from "./SidebarData";
+import { NavLink } from "react-router-dom";
+import classNames from 'classnames';
 
 function Sidebar() {
   return (
     <div className="Sidebar">
-        <ul className="SidebarList">
-            {SidebarData.map((val, key) => {
-                return (
-                    <li 
-                        className="row"
-                        key={key} 
-                        id={window.location.pathname == val.link ? "active" : ""}
-                        onClick={() => {window.location.pathname = val.link}}>
-                        <div id="icon">{val.icon}</div>
-    
-                        <div id="title">{val.title}</div>
-                    </li>
-                ) 
-            })}
-        </ul>
+      <ul className="SidebarList">
+        {SidebarData.map((val, key) => {
+          return (
+            <NavLink
+              className={({ isActive }) => {
+                return classNames("row", {
+                  ["activeRow"]: isActive,
+                });
+              }}
+              key={key}
+              to={val.link}
+            >
+              <div className="icon">{val.icon}</div>
+              <div className="title">{val.title}</div>
+            </NavLink>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Sidebar;
